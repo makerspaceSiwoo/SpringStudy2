172960e94abf7fd78ea6eb96fba43c7589cc9680
siwoo <128338682+makerspaceSiwoo@users.noreply.github.com>
2024-06-18 12:55:36 +0900

    Update FileinfoDao.java

--- file/dao/FileinfoDao.java
+++ file/dao/FileinfoDao.java
@@ -1,7 +1,10 @@
 package com.spring.file.dao;
 
-import org.apache.ibatis.annotations.Insert;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.spring.file.dto.FileinfoDto;
 
@@ -9,4 +12,9 @@ import com.spring.file.dto.FileinfoDto;
 public interface FileinfoDao {
 
 	int insertFile(FileinfoDto dto);//Mapper 아이디랑 똑같은 이름의 메소드
+	
+	List<FileinfoDto> fileList();
+	
+	@Select("select * from fileinfo where fileid=#{fileid}")
+	FileinfoDto fileOne(@Param("fileid") int id);
 }
