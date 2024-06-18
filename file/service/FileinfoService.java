30ebc0094622afb239eabe5062e705a431a39527
siwoo <128338682+makerspaceSiwoo@users.noreply.github.com>
2024-06-18 12:55:08 +0900

    Update FileinfoService.java

--- file/service/FileinfoService.java
+++ file/service/FileinfoService.java
@@ -1,5 +1,7 @@
 package com.spring.file.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,4 +20,11 @@ public class FileinfoService {
 		return x;
 	}
 	
+	public List<FileinfoDto> fileList(){
+		return fdao.fileList();
+	}
+	
+	public FileinfoDto fileOne(int id) {
+		return fdao.fileOne(id);
+	}
 }
